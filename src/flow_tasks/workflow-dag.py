2af49b2d08a3cbcf86f277355820999b296acf8f
André Carvalho <afacarvalho@yahoo.com.br>
2025-05-13 14:50:07 -0300

    Adopt the GENERAL_EMAIL_TO variable to send emails and other adjusts.

--- src/flow_tasks/workflow-dag.py
+++ src/flow_tasks/workflow-dag.py
@@ -1,9 +1,10 @@
 """A DAG to update the TerraBrasilis Active Fires database."""
-from datetime import datetime, timedelta
+from datetime import datetime
 import pathlib
 import sys
 import pendulum
 from airflow import DAG
+from airflow.models import Variable
 
 # used to load the location where base dag is
 dag_dir = str(pathlib.Path(__file__).parent.resolve().absolute())
@@ -15,28 +16,41 @@ sys.path.append(project_dir)
 
 from fires_dag_operators import BaseDagOperators
 
-DAG_KEY = "general_fires_data"
-venv_path=f"/opt/airflow/venv/inpe/{DAG_KEY}"
+# get a list of emails to send to, as a string in this format: email1@host.com,email2@host.com,email3@host.com
+EMAIL_TO = Variable.get("GENERAL_EMAIL_TO")
+if not EMAIL_TO:
+    raise Exception(
+        f"Missing GENERAL_EMAIL_TO variable into airflow configuration."
+    )
 
-default_args = {
-    "owner": "airflow",
+DAG_KEY = "active_fires_update"
+venv_path = f"/opt/airflow/venv/inpe/{DAG_KEY}"
+
+EMAIL_TO = str(EMAIL_TO).split(",")
+# Default arguments for all tasks. Precedence is the value at task instantiation.
+task_default_args = {
     "start_date": pendulum.datetime(year=2025, month=2, day=20, tz="America/Sao_Paulo"),
-    "email": ["afa.decarvalho@gmail.com"],
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": EMAIL_TO,
     "email_on_failure": True,
     "email_on_retry": False,
-    "retry_delay": timedelta(minutes=5),
     "retries": 0,
-    "dagrun_timeout": timedelta(minutes=1)
 }
 
 # apply 'catchup':False to prevent backfills
 with DAG(
-    DAG_KEY, catchup=False, max_active_runs=1, schedule_interval="10 01 * * *", default_args=default_args
+    DAG_KEY,
+    description="Update TerraBrasilis' active fire database",
+    catchup=False,
+    max_active_runs=1,
+    schedule_interval="10 01 * * *",
+    default_args=task_default_args,
 ) as dag:
 
     baseDag = BaseDagOperators(venv_path=venv_path, project_dir=project_dir)
-    
+
     update_current_task = baseDag.update_current_task_operator()
     update_last_task = baseDag.update_last_task_operator()
-    email_operator = baseDag.email_operator(datetime.today().strftime('%Y-%m-%d'))
+    email_operator = baseDag.email_operator(datetime.today().strftime("%Y-%m-%d"), email_to=EMAIL_TO)
     update_current_task >> update_last_task >> email_operator
